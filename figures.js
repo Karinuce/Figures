82c9ecc0b4dd1aee3b644dd1780623181621dd5b
Karinuce <94805043+Karinuce@users.noreply.github.com>
2022-08-04 11:59:10 +0300

    Add files via upload

--- figures.js
+++ figures.js
@@ -1,22 +1,24 @@
 const triangle = document.querySelector('[name="triangle"]');
-triangle.addEventListener('click', function(event){
-let n = 5;
-let string = "";
+const result = document.querySelector(".result");
+triangle.addEventListener("click", function (event) {
+    let n = 5;
+    let string = "";
     for (let i = 1; i <= n; i++) {
-    for (let j = 0; j < n - i; j++) {
-        string += " ";
-    }
     for (let k = 0; k < i; k++) {
         string += "*";
     }
-    string += "\n";
+        string += "\n";
+        string += "<br>";
+        result.innerHTML = string;
     }
-});
+    console.log(string);
+  });
 
 const pyramid = document.querySelector('[name="pyramid"]');
+const result1 = document.querySelector(".result");
 pyramid.addEventListener('click', function(event){
-let n = 5;
-let string = "";
+    let n = 5;
+    let string = "";
     for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n - i; j++) {
         string += " ";
@@ -25,22 +27,32 @@ let string = "";
         string += "*";
 }
         string += "\n";
+        string += "<br>";
+        result.innerHTML = string; 
 }
 console.log(string);
 });
 
 const reversepyramid = document.querySelector('[name="reverse-pyramid"]');
+const result2 = document.querySelector(".result");
 reversepyramid.addEventListener('click', function(event){
-let n = 5;
-let string = "";
+    let n = 5;
+    let string = "";
     for (let i = 0; i < n; i++) {
     for (let j = 0; j < i; j++) {
         string += " ";
 }
-for (let k = 0; k < 2 * (n-i) - 1; k++) {
+    for (let k = 0; k < 2 * (n-i) - 1; k++) {
         string += "*";
 }
         string += "\n";
+        string += "<br>";
+        result.innerHTML = string;
 }
 console.log(string);
 });
+
+
+function clearScreen(){
+    document.getElementsByClassName(".result").innerHTML = ""
+}
